4d4979216b16c2fa2217c68c09f3ddfda200b8fb
litzh <litzh@foxmail.com>
2024-12-12 15:52:17 +0800

    支持剪切板历史

--- init.lua
+++ init.lua
@@ -1,6 +1,15 @@
 logger = hs.logger.new('init.lua', 'debug')
+
 hs.loadSpoon("ReloadConfiguration")
 spoon.ReloadConfiguration:start()
+
+hs.loadSpoon("ClipboardTool")
+spoon.ClipboardTool.show_in_menubar = false
+spoon.ClipboardTool.show_copied_alert = false
+spoon.ClipboardTool:start()
+spoon.ClipboardTool:bindHotkeys({
+  toggle_clipboard = {{'cmd', 'shift'}, 'X'},
+})
 
 function commonEvent(module, event,  message, data)
     logger.i(string.format("%s: %s: %s", module, event, message))
