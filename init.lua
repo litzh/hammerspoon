98078b65bc66aab7126c03d03874976f95880271
litzh <litzh@foxmail.com>
2024-12-12 14:58:35 +0800

    remove SwitchHosts

--- init.lua
+++ init.lua
@@ -2,31 +2,6 @@ logger = hs.logger.new('init.lua', 'debug')
 hs.loadSpoon("ReloadConfiguration")
 spoon.ReloadConfiguration:start()
 
-local SwitchHosts = require("SwitchHosts")
-SwitchHosts.proxyGroup = {
-    Default = {"GitHub520", "Onedrive"},
-    Home = {"AX89X"},
-    Office = {"Hetao101"},
-    Feilian = {"Testing0"},
-    WireGuard = {"WireGuard"},
-}
-
-function arriveHome()
-    SwitchHosts.switchGroup("Home", true)
-end
-
-function leaveHome()
-    SwitchHosts.switchGroup("Home", false)
-end
-
-function arriveOffice()
-    SwitchHosts.switchGroup("Office", true)
-end
-
-function leaveOffice()
-    SwitchHosts.switchGroup("Office", false)
-end
-
 function commonEvent(module, event,  message, data)
     logger.i(string.format("%s: %s: %s", module, event, message))
     hs.notify.new({title=module, informativeText=string.format("%s\n%s", event, message)}):send()
