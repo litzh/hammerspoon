8c05c006b7c307fa2f9d9a62a674c47a46acb644
litzh <litzh@foxmail.com>
2024-12-31 15:17:02 +0800

    remove ClipboardTool

--- init.lua
+++ init.lua
@@ -3,14 +3,6 @@ logger = hs.logger.new('init.lua', 'debug')
 hs.loadSpoon("ReloadConfiguration")
 spoon.ReloadConfiguration:start()
 
-hs.loadSpoon("ClipboardTool")
-spoon.ClipboardTool.show_in_menubar = false
-spoon.ClipboardTool.show_copied_alert = false
-spoon.ClipboardTool:start()
-spoon.ClipboardTool:bindHotkeys({
-  toggle_clipboard = {{'cmd', 'shift'}, 'X'},
-})
-
 function commonEvent(module, event,  message, data)
     logger.i(string.format("%s: %s: %s", module, event, message))
     hs.notify.new({title=module, informativeText=string.format("%s\n%s", event, message)}):send()
